6adafabcf1be40589c279ab376b17bc9849306ee
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-15 18:50:35 -0300

    solicitaIdadeClassifica

--- exercicio1.js
+++ exercicio1.js
@@ -6,3 +6,20 @@
  * adulto (18-59 anos) ou idoso (60 anos ou mais). Utiliza uma estrutura condicional
  * (if, else if, else).
  */
+let idade = prompt ("Digite sua idade");
+idade = parseInt(idade);
+
+if (idade >= 0) {
+    if (idade <= 12) {
+        console.log ("Criança. ")
+    }else if (idade > 12 && idade <= 17){
+        console.log ("Adolescente. ");
+    }else if ( idade >17 && idade <= 59){
+        console.log (" Adulto. ")
+    }else{
+        console.log ("Idoso. ")
+    }
+
+}else{
+    console.log ("Idade precisa ser positiva. ")
+}
