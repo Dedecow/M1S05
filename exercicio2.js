7ec223cf13cbfb4417ec82f0f6c68ed180bd8a34
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-15 20:21:22 -0300

    coresDoArcoiris

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,30 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+let cor = prompt ("Digite a cor do Arco-íris a ser traduzida: ");
+
+switch (cor.toLowerCase()){
+    case "red":
+    console.log("Vermelho");
+    break;
+    case "orange":
+    console.log("Laranja");
+    break;
+    case "yellow":
+    console.log("Amarelo");
+    break;
+    case "green":
+    console.log("Verde");
+    break;
+    case "blue":
+    console.log ("Azul");
+    break;
+    case "indigo": case "dark blue": case "navy blue":
+    console.log("Anil (ou ciano). ");
+    break;
+    case: "Violet":
+    console.log("Violeta");
+    break;
+  default:
+    console.log("Valor informado não é uma cor do Arco-iris. ");
+}
