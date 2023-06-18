5444c2b2058b374f458e33150fd4fee0078868ac
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-18 17:02:57 -0300

    logicaOK

--- exercicio6.js
+++ exercicio6.js
@@ -1,7 +1,15 @@
 /**
  * [M1S05] Ex 6 - Percorrer array de objetos de produtos
  *
- * Escreve um programa que percorre um array de objetos que representam produtos e exibe o nome
- * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
+ * Escreve um programa que percorre um array de objetos
+ *  que representam produtos e exibe o nome
+ * e o preço de cada produto. 
+ * Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+let farmacos = [ Paracetamol, Ibuprofeno, Aspirina, Loratadina, Cetirizina]
+let precos = [ 7.89, 13.19, 18.19, 15.29, 49.90]
+
+for (let i=0; i < farmacos.length; i++){
+    console.log("O preço do " + farmacos[i] + "É R$" + precos[i]);
+}
