6c6c6ce39fa248bf5e0b650d14d9b3f96fd7bb8b
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-19 09:40:20 -0300

    primeiroCommit

--- exercicio8.js
+++ exercicio8.js
@@ -5,3 +5,26 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+
+const calculadora = function(num1, num2, operation){
+
+};
+
+switch (operation) {
+    case "+":
+      // Operação de soma
+      break;
+    case "-":
+      // Operação de subtração
+      break;
+    case "*":
+      // Operação de multiplicação
+      break;
+    case "/":
+      // Operação de divisão
+      break;
+    default:
+      console.log ("Operação inválida. ")
+      break;
+  }
+  
