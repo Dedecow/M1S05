6f90cc0ca480d1ba7903f2a3ad56ec0ffe8d3030
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-19 04:44:17 -0300

    Refeito na forma de objeto

--- exercicio6.js
+++ exercicio6.js
@@ -7,9 +7,24 @@
  * Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
-let farmacos = [ Paracetamol, Ibuprofeno, Aspirina, Loratadina, Cetirizina]
-let precos = [ 7.89, 13.19, 18.19, 15.29, 49.90]
 
-for (let i=0; i < farmacos.length; i++){
-    console.log("O preço do " + farmacos[i] + "É R$" + precos[i]);
-}
+function criaFarmaco (farmaco, preco){
+    return{
+        farmaco:farmaco;
+        preco:preco;
+    };
+};
+const farmaco1 = criaFarmaco ('Paracetamol' , 7.89);
+const farmaco2 = criaFarmaco ('Ibuprofeno' , 13.19);
+const farmaco3 = criaFarmaco ('Aspirina' , 18.19);
+const farmaco4 = criaFarmaco ('Loratadina' , 15.29);
+const farmaco5 = criaFarmaco ('Cetirizina' , 49.90);
+const farmaco6 = criaFarmaco ('Dipirona' , 9.99);
+const farmaco7 = criaFarmaco('Omeprazol', 25.99);
+const farmaco8 = criaFarmaco('Amoxicilina', 12.49);
+const farmaco9 = criaFarmaco('Ranitidina', 19.79);
+
+
+//for (let i=0; i < farmacos.length; i++){
+//     console.log("O preço do " + farmacos[i] + "É R$" + precos[i]);
+//}
